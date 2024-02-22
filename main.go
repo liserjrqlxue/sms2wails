2d920d0b94e8e14b7fc5883966b6d0d48284d1a0
Wang Yaoshen <wangyaoshen@zhonghegene.com>
2024-02-22 09:40:02 +0800

    💄 🔥 clean and use acrylic

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/src
@@ -13,7 +14,7 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	// app := NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
@@ -23,10 +24,18 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		BackgroundColour: &options.RGBA{R: 255, G: 0, B: 0, A: 0},
+		Windows: &windows.Options{
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			BackdropType:         windows.Acrylic,
+			// BackdropType: windows.Tabbed,
+			// BackdropType: windows.Mica,
+			EnableSwipeGestures: true,
+		},
+		// OnStartup:        app.startup,
 		Bind: []interface{}{
-			app,
+			// app,
 		},
 	})
 
